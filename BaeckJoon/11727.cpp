a3c810835c21f80fe47026fd93df2afb03b81064
brian <kimchanjoong54@gmail.com>
2025-07-30 15:29:16 +0900

    11727 solved (silver 3)

---
+++ BaeckJoon/11727.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cstring>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int dp[1001];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+    }
+    
+    cout << dp[n] % 10007;
+
+    return 0;
+}
