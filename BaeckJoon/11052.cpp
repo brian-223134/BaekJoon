a8c597a9775fc99800dafea92aaa9739c8558d36
brian <kimchanjoong54@gmail.com>
2025-08-11 14:33:19 +0900

    11052 solved (silver 1)

---
+++ BaeckJoon/11052.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int p[1001];
+int dp[1001];
+
+int main(void) 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+    {
+        cin >> p[i];
+    }
+
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            dp[i] = max(dp[i], dp[i - j] + p[j]);
+        }
+    }
+
+    cout << dp[n] << '\n';
+    return 0;
+}
