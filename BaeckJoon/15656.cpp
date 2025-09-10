c5a1e8ed0581e477c841b958321868d49e90ca5e
brian <kimchanjoong54@gmail.com>
2025-09-10 18:10:58 +0900

    15656 solved (silver 3)

---
+++ BaeckJoon/15656.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cstring>
+
+using namespace std;
+
+int n, m;
+int numbers[7];
+int arr[7];
+
+void solution(int depth, int start)
+{
+    if (depth == m) 
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i < n; i++)
+    {
+        arr[depth] = numbers[i];
+        solution(depth + 1, start);
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    sort(numbers, numbers + n);
+    solution(0, 0);
+
+    return 0;
+}
